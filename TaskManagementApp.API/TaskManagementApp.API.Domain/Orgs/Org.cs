1f39d99feb43c749bf07c9723d93fa675c4f97d1
ian6ft5 <irogers@bullfrogspas.com>
2024-01-10 12:59:43 -0700

    domain v0.0

---
+++ TaskManagementApp.API/TaskManagementApp.API.Domain/Orgs/Org.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManagementApp.API.Domain.Users;
+
+namespace TaskManagementApp.API.Domain.Orgs
+{
+    public class Org
+    {
+        public int OrgId { get; set; }
+        public string OrgName { get; set; }
+        public List<User> Members { get; set; }
+    }
+}
---
+++ TaskManagementApp.API/TaskManagementApp.API.Domain/Tasks/AppTask.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManagementApp.API.Domain.Users;
+
+namespace TaskManagementApp.API.Domain.Tasks
+{
+    public class AppTask
+    {
+        public int AppTaskID { get; set; }
+        public string TaskName { get; set; }
+        public string TaskDescription { get; set; }
+        public string Status { get; set; }
+        public List<ChecklistItem> ChecklistItems { get; set; }
+        public User AssignedTo { get; set; }
+    }
+}
